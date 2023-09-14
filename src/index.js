5b51aa4ca738f2334ad86c80cc2a8f30dd7a3630
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-09-14 13:42:16 -0300

    adicionando redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import Navbar from './components/Navbar';
 import './index.css';
 import Router from './routes';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
+      <Provider>
       <Router/>
+      </Provider>
+    
     </>
   </React.StrictMode>
 );

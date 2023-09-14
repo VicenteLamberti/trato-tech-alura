83703d9fc0964b79fd2135d9769748c4cb177d7b
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-09-14 16:08:47 -0300

    mostrando itens

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,15 +1,14 @@
 import Header from "../../components/Header";
 import {useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
+import styles from './Categoria.module.scss';
 
 export default function Categoria(){
     const {nomeCategoria} = useParams();
-    const {categoria, itens} = useSelector(state=>{
-        return{
-            categoria:state.categorias.find(categoria=>categoria.id === nomeCategoria),
-            itens:state.itens.filter(item=>item.categoria ===nomeCategoria)
-        }
-    });
+    const {categoria, itens} = useSelector(state=>({
+        categoria:state.categorias.find(categoria=>categoria.id === nomeCategoria),
+        itens:state.itens.filter(item=>item.categoria ===nomeCategoria)
+    }));
     console.log( itens)
     return(
         <div>
@@ -17,6 +16,12 @@ export default function Categoria(){
             titulo={categoria.nome}
             descricao={categoria.descricao}
             imagem={categoria.header}/>
+            <div className={styles.itens}>
+                {itens?.map(item=>(
+                    <div key={item.id}>{item.titulo}</div>
+                ))}
+            </div>
         </div>
+
     )
 }
